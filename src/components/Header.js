bba009e0e94d1276e417569517a4a98c6a8db831
SphereSRYN <sphere.sryn@gmail.com>
2022-03-22 15:32:56 +0900

    Add Remaining navMenus in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,32 @@ const Header = (props) => {
       <Logo>
         <img src="./images/logo.svg" alt="Disney plus" />
       </Logo>
+      <NavMenu>
+        <a href="/home">
+          <img src="./images/home-icon.svg" alt="home" />
+          <span>HOME</span>
+        </a>
+        <a href="/search">
+          <img src="./images/search-icon.svg" alt="search" />
+          <span>SEARCH</span>
+        </a>
+        <a href="/watchlist">
+          <img src="./images/watchlist-icon.svg" alt="watchlist" />
+          <span>WAHTCHLIST</span>
+        </a>
+        <a href="/original">
+          <img src="./images/original-icon.svg" alt="original" />
+          <span>ORIGINALS</span>
+        </a>
+        <a href="/movie">
+          <img src="./images/movie-icon.svg" alt="movie" />
+          <span>MOVIES</span>
+        </a>
+        <a href="/series">
+          <img src="./images/series-icon.svg" alt="series" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
     </Nav>
   );
 };
@@ -37,5 +63,87 @@ const Logo = styled.a`
     display: block;
     width: 100%;
   }
+`;
+
+const NavMenu = styled.div`
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  margin: 0;
+  padding: 0;
+  position: relative;
+  margin-right: auto;
+  margin-left: 25px;
+
+  a {
+    display: flex;
+    padding: 0 12px;
+    align-items: center;
+
+    img {
+      height: 20px;
+      min-width: 20px;
+      width: 20px;
+      z-index: auto;
+    }
+
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      padding: 2px 0px;
+      white-space: nowrap;
+      position: relative;
+
+      &:before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0px 0px 4px 4px;
+        bottom: -5px;
+        content: "";
+        height: 2px;
+        left: 0px;
+        right: 0px;
+        opacity: 0;
+        position: absolute;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
+    }
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
+    }
+  }
+
+  /* &:before {
+    content: "hello";
+    display: block;
+    background-color: rgb(249, 249, 249);
+    border-radius: 0px 0px 4px 4px;
+    bottom: -6px;
+    content: "";
+    height: 2px;
+    opacity: 0;
+    position: absolute;
+    right: 0px;
+    transform-origin: left center;
+    transform: scaleX(0);
+    transition: all 250ms cubic-bezier(0.25, 046 0.45, 0.94);
+    visibility: hidden;
+    width: auto;
+  } */
+
+  /* @media (max-width: 768px) {
+    display: none;
+  } */
 `;
 export default Header;
