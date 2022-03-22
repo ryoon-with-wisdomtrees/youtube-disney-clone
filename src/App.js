f19225a6524b3a05786fd69993ddcce873682e83
SphereSRYN <sphere.sryn@gmail.com>
2022-03-22 14:00:56 +0900

    _

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
-
+import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <switch>
           <Route exact path="/">
             <Login test="바부" />
